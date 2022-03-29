236773d76c49cb3316b3786c7e7a8580af1d80fb
Gustavo Peixoto <gustavoanrde@hotmail.com>
2022-03-29 11:59:06 -0300

    Create tabuada_while_aninhado.py

---
+++ tabuada_while_aninhado.py
@@ -0,0 +1,11 @@
+# programa de tabuada com duas condições while aninhadas
+
+tabuada = 1
+while tabuada <= 10:
+    numero = 1
+    while numero <= 10:
+        print(f'{tabuada} x {numero} = {tabuada * numero}')
+        numero += 1
+    tabuada += 1
+
+# fim do programa
