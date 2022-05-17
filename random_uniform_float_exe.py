3d61a34586990423cf5e0cab4ce5341beed96f5d
Gustavo Peixoto <gustavoanrde@hotmail.com>
2022-05-17 13:51:28 -0300

    Create random_uniform_float_exe.py

---
+++ random_uniform_float_exe.py
@@ -0,0 +1,8 @@
+# Exercício usando a função uniform do módulo random
+
+import random
+
+for i in range(10):
+    print(random.uniform(1, 10)) # a função uniform retorna valores float aleatórios entre o range que ela utiliza.
+
+# fim do programa
