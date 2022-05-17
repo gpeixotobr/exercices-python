e342660d42307d42e709bd11ea2d267d0fc4a896
Gustavo Peixoto <gustavoanrde@hotmail.com>
2022-05-17 13:47:58 -0300

    Create random__shuffle_exe.py

---
+++ random__shuffle_exe.py
@@ -0,0 +1,9 @@
+# Exercício usando a função shuffle para embaralhar elementos de unma lista
+
+import random
+
+L = list(range(1,11))
+random.shuffle(L) # a função shuffle do módulo random retorna os valores de uma lista embaralhados aleatoriamente
+print(L)
+
+# fim do programa
